1e7d2fcc9366bd385d7276639521ebbe25993400
gregdepaul <gdepaul@stanford.edu>
2019-11-04 10:02:55 -0800

    Added Dynamical System Analyzers

---
+++ My2DSystemAnalyzer.m
@@ -0,0 +1,205 @@
+function [] = My2DSystemAnalyzer() 
+
+    a = -2;
+    b = -3;
+
+    % Specify System
+    x_dot = @(x, y) x - y;
+    y_dot = @(x, y) x + y;
+    
+    % Constants
+    epsilon = 10^(-3); % Degree of allowable similarity between fixed points identified numerically
+    h = 0.01; % Resolution for vector field
+    r = 1; % Bounding box radius around each fixed point
+    vector_normalizer = 3; % Divide the vectors drawn for decoupled fixed points
+    
+    % Draw Real Vectos
+    draw_real_eigenvectors = 1;
+    
+    % Override Plot Bounds
+    override_bounds = 0;
+    new_plot_bounds = [-1 1 -0.5 0.5];
+    
+    % Override Numerical Solver Params
+    % NOTICE: Turn off when dealing with purely polynomial equations
+    numerical_override = 0;
+    numerical_search_range = [ -3*pi 3*pi; -2 2];
+    number_of_possible_fixed_points = 11;
+
+    % Identify Fixed Points
+    syms x y x_0 y_0 t
+    assume(x, 'real');
+    assume(y, 'real');
+    f = x_dot(x,y);
+    g = y_dot(x,y);
+    eqn1 = f == 0;
+    eqn2 = g == 0;
+    eqns = [eqn1, eqn2];
+    if numerical_override == 1
+        S = vpasolve(eqns, [x y], numerical_search_range);
+        clear sol
+        j = 1;
+        for i = 1:(number_of_possible_fixed_points^2)
+            temp = vpasolve(eqns, [x y], numerical_search_range, 'random', true);
+            if ~isempty(temp.x)
+                sol(i, 1) = temp.x;
+                sol(i, 2) = temp.y;
+                j = j + 1;
+            end
+        end
+        
+        % Sort Solution Array
+        if j > 1
+            sol = unique(sol, 'rows');
+
+            % Identify Repeated Rows
+            clear A
+            A = zeros(length(sol));
+            for i = length(sol):-1:2
+                if abs(sol(i, 1) - sol(i-1, 1)) < epsilon && abs(sol(i, 2) - sol(i-1, 2)) < epsilon
+                    A(i) = 1;
+                end
+            end
+
+            % Isolate Unique Rows
+            unique_x_values = zeros(length(sol) - length(A));
+            unique_y_values = zeros(length(sol) - length(A));
+            j = 1;
+            for i = 1:length(sol)
+                if A(i) == 0
+                    unique_x_values(j) = sol(i,1);
+                    unique_y_values(j) = sol(i,2);
+                    j = j + 1;
+                end
+            end
+
+            % Place In Struct
+            S.x = unique_x_values;
+            S.y = unique_y_values;
+        end
+    else
+        S = solve(eqns,[x y]);
+    end
+    
+    % Perform Calculations About Each Fixed Point
+    if isempty(S.x)
+        min_X = -1;
+        max_X = 1;
+        min_Y = -1;
+        max_Y = 1;
+    else
+        min_X = 100;
+        max_X = -100;
+        min_Y = 100;
+        max_Y = -100;
+        for i = 1:length(S.x)    
+            if S.x(i) > max_X
+                max_X = S.x(i);
+            end
+            if S.x(i) < min_X
+                min_X = S.x(i);
+            end
+            if S.y(i) > max_Y
+                max_Y = S.y(i);
+            end
+            if S.y(i) < min_Y
+                min_Y = S.y(i);
+            end
+            J = [diff(f,x) diff(f,y); diff(g,x) diff(g,y)];
+            J = subs(subs(J, x, S.x(i)), y, S.y(i)); 
+            [V,D] = eig(J);
+            eigenvalues = diag(D)
+            latex(V(:,1))
+            latex(V(:,2))
+            %c = V^(-1)*[x_0; y_0];
+            %latex(c(1)*exp(D*t)*V(:,1) + c(2)*exp(D*t)*V(:,2))
+            
+            if eigenvalues == real(eigenvalues)
+                
+                if eigenvalues(1) ~= eigenvalues(2)
+                    % Distinct Real Eigenvalues
+                    if min(eigenvalues) > 0
+                        fprintf('(%.2f, %.2f) is an unstable node \n',S.x(i), S.y(i));
+                    elseif max(eigenvalues) < 0
+                        fprintf('(%.2f, %.2f) is a stable node \n',S.x(i), S.y(i));
+                    else
+                        fprintf('(%.2f, %.2f) is a saddle point \n',S.x(i), S.y(i));
+                    end
+                else
+                    eigenvectors_size = size(V);
+                    % Repeated Eigenvalues
+                    if eigenvectors_size(2) == 1
+                        % Emits a Single Eigenvectors
+                        if eigenvalues(1) > 0
+                            fprintf('(%.2f, %.2f) is an unstable degenerate node \n',S.x(i), S.y(i));
+                        else
+                            fprintf('(%.2f, %.2f) is a stable degenerate node \n',S.x(i), S.y(i));
+                        end
+                    else
+                        % Two Linearly Independent Eigenvectors
+                        fprintf('(%.2f, %.2f) is a star node \n',S.x(i), S.y(i));
+                    end
+                end
+                
+                if draw_real_eigenvectors == 1
+                    eigenvectors_size = size(V);
+
+                    myColor = [rand() rand() rand()];
+                    for k=1:eigenvectors_size(2)
+                        quiver(double(S.x(i)), double(S.y(i)), V(1, k)/vector_normalizer, V(2,k)/vector_normalizer,'linewidth',5,'color',myColor)
+                        hold on
+                        quiver(double(S.x(i)), double(S.y(i)), -V(1, k)/vector_normalizer, -V(2,k)/vector_normalizer,'linewidth',5,'color',myColor)
+                        hold on
+                    end
+                end
+            else
+                % Complex Eigenvalues
+                real_component = real(eigenvalues(1));
+                if real_component > 0
+                    fprintf('(%.2f, %.2f) is an unstable spiral \n',S.x(i), S.y(i));
+                elseif real_component < 0
+                    fprintf('(%.2f, %.2f) is a stable spiral \n',S.x(i), S.y(i));
+                else
+                    fprintf('(%.2f, %.2f) is a center \n',S.x(i), S.y(i));
+                end
+            end
+        end
+    end
+    
+    % Account for resolution
+    min_X = min_X - r; 
+    max_X = max_X + r;
+    min_Y = min_Y - r; 
+    max_Y = max_Y + r;
+
+    % In case bounds are still bad
+    if override_bounds == 1
+        min_X = new_plot_bounds(1);
+        max_X = new_plot_bounds(2);
+        min_Y = new_plot_bounds(3);
+        max_Y = new_plot_bounds(4);
+    end
+    
+    % Draw Vector Field
+    X_ = double(min_X):h:double(max_X);
+    Y_ = double(min_Y):h:double(max_Y);
+    [X, Y] = meshgrid(X_,Y_);
+    Z_1 = arrayfun(x_dot, X, Y);
+    Z_2 = arrayfun(y_dot, X, Y);
+    streamslice(X,Y, Z_1, Z_2);
+    axis([double(min_X) double(max_X) double(min_Y) double(max_Y)])
+    
+      %%%%%%%%%%%%%%%%%%%%%%%%%
+    % Axis Preferences
+    %%%%%%%%%%%%%%%%%%%%%%%%%
+    hl = xlabel('$x$');
+    set(hl, 'Interpreter', 'latex');
+    hl = ylabel('$y$');
+    set(gca,'FontSize',20);
+    set(hl, 'Interpreter', 'latex');
+    %hl = title('$x^*$ vs $h$');
+    %set(hl, 'Interpreter', 'latex');
+    %set(gca,'xtick',[])
+    %set(gca,'ytick',[])
+    
+end
---
+++ My3DSystemAnalyzer.m
@@ -0,0 +1,72 @@
+function [] = My3DSystemAnalyzer() 
+
+    % Specify System
+    x_dot = @(x, y, z) -2*x + 1*y + 0*z;
+    y_dot = @(x, y, z) 0*x + 1*y + 1*z;
+    z_dot = @(x, y, z) 0*x + 4*y + -2*z;
+    
+    % Constants
+    epsilon = 10^(-3); % Degree of allowable similarity between fixed points identified numerically
+    h = 0.01; % Resolution for vector field
+    r = 4; % Bounding box radius around each fixed point
+    num_curves = 20; % Number of curves to be drawn
+
+    % Identify Fixed Points
+    syms x y z
+    assume(x, 'real');
+    assume(y, 'real');
+    assume(z, 'real');
+    f = x_dot(x,y,z);
+    g = y_dot(x,y,z);
+    h = z_dot(x,y,z);
+    eqn1 = f == 0;
+    eqn2 = g == 0;
+    eqn3 = h == 0;
+    eqns = [eqn1, eqn2, eqn3];
+    S = solve(eqns,[x y z]);
+    
+    for i = 1:length(S.x) 
+        J = [diff(f,x) diff(f,y), diff(f,z); diff(g,x) diff(g,y), diff(g,z); diff(h,x) diff(h,y), diff(h,z)];
+        J = subs(subs(subs(J, x, S.x(i)), y, S.y(i)),z,S.z(i)); 
+        [V,D] = eig(J);
+        eigenvalues = diag(D)
+        latex(V(:,1))
+        latex(V(:,2))
+        latex(V(:,3))
+        for j=1:num_curves
+            a = [double(S.x(i)) double(S.y(i)) double(S.z(i))] + [r*(rand()-0.5) r*(rand()-0.5) r*(rand()-0.5)];
+            f = @(t,x)[x_dot(x(1), x(2), x(3))
+                y_dot(x(1), x(2), x(3))
+                z_dot(x(1), x(2), x(3))];
+            t=linspace(0,1,200);
+            [t,x]=ode45(f,t,a);
+            cm = jet(numel(t));
+            hold on
+            for k = 1:size(x,1)-1
+                hl = plot3([x(k,1),x(k+1,1)], [x(k,2),x(k+1,2)], [x(k,3),x(k+1,3)]);
+                set(hl, 'LineStyle','-', 'Color',[0 0 0]);
+            end
+            hold off
+            grid on
+            view(50, 30)
+        end
+    end
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%
+    % Axis Preferences
+    %%%%%%%%%%%%%%%%%%%%%%%%%
+    hl = xlabel('$x$');
+    set(hl, 'Interpreter', 'latex');
+    hl = ylabel('$y$');
+    set(hl, 'Interpreter', 'latex');
+    hl = zlabel('$z$');
+    set(hl, 'Interpreter', 'latex');
+    set(gca,'FontSize',20);
+    
+    %hl = title('$x^*$ vs $h$');
+    %set(hl, 'Interpreter', 'latex');
+    %set(gca,'xtick',[])
+    %set(gca,'ytick',[])
+    
+end
